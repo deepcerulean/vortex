68b6e6f67e82fa8e6b1b684dbe39c43354fb1b54
Joseph Weissman <joe@bignerdranch.com>
2016-05-05 11:57:58 -0400

    try to log out what is happening with redis conn

--- lib/vortex/extend/metacosm/simulation.rb
+++ lib/vortex/extend/metacosm/simulation.rb
@@ -1,10 +1,13 @@
 module Metacosm
   class Simulation
     def redis_connection
+      puts "---> establishing redis connection..."
       if ENV['REDISTOGO_URL']
+        puts "---> using redis to go!"
         uri = URI.parse(ENV["REDISTOGO_URL"])
         Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
       else
+        puts "---> using default redis settings..."
         super
       end
     end
