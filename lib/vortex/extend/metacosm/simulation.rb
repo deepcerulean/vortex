913456b393ba670822b7fe5ef00bf4c0d73aecda
Joseph Weissman <joe@bignerdranch.com>
2016-05-05 12:04:43 -0400

    we are not using resque :/

--- lib/vortex/extend/metacosm/simulation.rb
+++ lib/vortex/extend/metacosm/simulation.rb
@@ -5,7 +5,8 @@ module Metacosm
       if ENV['REDISTOGO_URL']
         puts "---> using redis to go!"
         uri = URI.parse(ENV["REDISTOGO_URL"])
-        Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+        puts "---> parsed uri: #{uri}"
+        Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
       else
         puts "---> using default redis settings..."
         super
