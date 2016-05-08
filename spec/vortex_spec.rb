8931b755fa1cf25847771f2ec184d25aacaa6cbf
Joseph Weissman <joe@bignerdranch.com>
2016-05-08 15:25:46 -0400

    fix specs

--- spec/vortex_spec.rb
+++ spec/vortex_spec.rb
@@ -36,7 +36,7 @@ describe JumpCommand do
   let(:player_jumped) do
     PlayerUpdatedEvent.create(
       player_id: 'the_player_id',
-      velocity: [0.0,-3],
+      velocity: [0.0,-9],
       color: 'color',
       updated_at: Time.now,
       game_id: 'the_game_id',
